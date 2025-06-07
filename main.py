6e31862533a90ecbef87dc64e16be2968fe60d42
Nadeem Ahmad <Nddar191131@gmail.com>
2025-06-07 09:17:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ TOKEN = os.getenv("DISCORD_TOKEN")
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = commands.Bot(command_prefix= "~",intents=intents)
+client = commands.Bot(command_prefix= "/",intents=intents)
 
 @client.event
 async def on_ready():
